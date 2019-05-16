8e15f2aa96a6ea22f73a9514d97f8f13912a4e7c
gravitation <chisong@ucdavis.edu>
2019-05-15 23:56:58 -0700

    working files

--- liri.js
+++ liri.js
@@ -1,3 +1,39 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var commands= process.argv[2];
+var thisSong = process.argv.slice(3).join(" ");
+
+switch (commands) {
+  case "concert-this":
+    concertThis();
+    break;
+
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;
+  case "movie-this":
+    movieThis();
+    break;
+
+  case "do-what-it-says":
+    doWhatItSays();
+    break;
+}
+
+function concertThis() {
+  console.log("hello")
+}
+
+function spotifyThisSong() {
+  console.log(spotify);
+  console.log(thisSong);
+}
+
+function movieThis() {
+
+}
+
+function doWhatItSays() {
+
+}
---
+++ spotify.js
@@ -0,0 +1,81 @@
+var Spotify = require('node-spotify-api');
+var axios = require("axios");
+var fs = require("fs");
+var commands = process.argv[2];
+var query = process.argv.slice(3).join(" ");
+var spotify = new Spotify({
+    id: "9baadd090c594485a28602557d0f348e",
+    secret: "9c5b1cce9bc04580966d71ea8d75a27d"
+});
+
+switch (commands) {
+    case "concert-this":
+        concertThis();
+        break;
+
+    case "spotify-this-song":
+        spotifyThisSong();
+        break;
+    case "movie-this":
+        movieThis();
+        break;
+
+    case "do-what-it-says":
+        doWhatItSays();
+        break;
+}
+
+function concertThis() {
+    console.log("hello")
+}
+// Working
+function spotifyThisSong() {
+    spotify.search({ type: 'track', query: query, limit:1}, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+      
+      //Artist(s)
+      console.log("Artist(s): " + data.tracks.items[0].album.artists[0].name);
+      //The song's name
+      console.log("Song title: " + data.tracks.items[0].album.name);
+      //A preview link of the song from Spotify
+      console.log("Spotify preview: " + data.tracks.items[0].album.artists[0].external_urls.spotify);
+      //The album that the song is from
+      console.log("Album title: " + data.tracks.items[0].album.name);
+      });    
+}
+// Working
+function movieThis() {
+    axios.get("http://www.omdbapi.com/?t=" + query + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+            //* Title of the movie
+            console.log("Title: " + response.data.Title);
+            //* Year the movie came out
+            console.log("Year: " + response.data.Year);
+            //* IMDB Rating of the movie.
+            console.log("IMDB rating: " + response.data.imdbRating);
+            //* Rotten Tomatoes Rating of the movie.
+            console.log("Rotten Tomatoes rating: " + response.data.Ratings[1].Value);
+            //* Country where the movie was produced.
+            console.log("Country produced: " + response.data.Released);
+            //* Language of the movie.
+            console.log("language: " + response.data.Language);
+            //* Plot of the movie.
+            console.log("plot: " + response.data.Plot);
+            //* Actors in the movie.
+            console.log("Actors: " + response.data.Actors);
+        }
+    );
+}
+
+function doWhatItSays() {
+    fs.readFile("random.txt", "utf8", function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.split(",")
+
+        console.log("node " + data);
+    });
+}
